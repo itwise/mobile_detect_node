306c298b2a68cb675ccf7e3251975fc1af00ce9f
thkim@itwise.co.kr <thkim@itwise.co.kr>
2015-01-23 11:02:20 +0900

    스크린 사이즈 얻는 부분 추가

--- public/js/deviceSizeChecker.js
+++ public/js/deviceSizeChecker.js
@@ -1,3 +1,4 @@
+'use strict';
 (function($){
    $(document).ready(function(){
        function renderingScreenInfo(){
@@ -9,15 +10,16 @@
                    devicePixelRatio = window.screen.deviceXDPI / window.screen.logicalXDPI;
                }
            }
-           var resultHTML = '<div id="screen-info"> 현재 기기의 해상도는 width:' + width + ', height:' + height +'입니다.<br>';
+           var resultHTML = '<div id="screen-info"> 현재 화면의 해상도는 width:' + width + ', height:' + height +'입니다.<br>';
            if(devicePixelRatio !== undefined){
                resultHTML = resultHTML + '기기픽셀 배율은 ' + devicePixelRatio + '입니다.<br>';
            }else{
                resultHTML = resultHTML + '기기픽셀 배율값을 얻어올 수 없습니다.<br>';
            }
 
+           resultHTML = resultHTML + '스크린의 해상도는 width:' + window.screen.width + ', height:' + window.screen.height + '입니다.<br>';
            resultHTML = resultHTML + 'User Agent: ' + window.navigator.userAgent;
-           
+
            if($('#screen-info').size() > 0){
                $('#screen-info').remove();
            }
